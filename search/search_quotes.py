5b4cf6176669647c4485c61dd07eccc1c6e490ab
alexfighterbomber <ssamsobi@gmail.com>
2025-03-29 16:15:45 +0200

    final

--- search/search_quotes.py
+++ search/search_quotes.py
@@ -3,7 +3,7 @@ import json
 from models.models import Author, Quote
 from services.connect import *
 
-redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
 def get_cached_data(key):
     """Отримуємо дані з Redis."""
